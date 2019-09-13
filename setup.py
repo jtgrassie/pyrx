70c3879d7ca6586797dce8093448574ff1c607fc
Jethro Grassie <jtg@xtrabass.com>
2019-09-13 18:42:48 -0400

    bump version

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='pyrx',
-    version='0.0.1',
+    version='0.0.2',
     author='Jethro Grassie',
     author_email='jtgrassie@users.noreply.github.com',
     description='Python RandomX hashing module',
